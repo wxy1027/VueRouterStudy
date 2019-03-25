72dda0a9728e3098f42873e866fe106cf223eca4
wxy1027 <2324407028@qq.com>
2019-03-25 20:56:22 +0800

    vue-router-demo1

--- vue-router-demo1/src/components/NewCourse.vue
+++ vue-router-demo1/src/components/NewCourse.vue
@@ -2,8 +2,21 @@
 	<div class="container">
 		<input type="text" placeholder="请输入班课名称" v-model="course.courseName" class="input-box" />
 		<input type="text" placeholder="请输入班级" v-model="course.courseClass" class="input-box" />
-		<input type="text" placeholder="请输入图片地址" v-model="course.cover" class="input-box" />
-		<button @click="addCourse(course)" class="btn">确定</button>
+		<p class="title">设置班课封面</p>
+		<div class="preview" @click="handleClick()">
+			<img :src="course.cover" class="course-cover" v-if="!show" />
+			<div class="icon-plus">
+				<img src="../assets/plus.png"  v-if="show" />
+			</div>
+				
+			
+			<input type="file" @change="getFile($event)" style="display: none;" id="coverFile" />
+		</div>
+		<div class="btn">
+			<button @click="addCourse(course)" class="btn1">确定</button>
+			<button class="btn2">取消</button>
+		</div>
+
 	</div>
 </template>
 
@@ -16,8 +29,10 @@
 				course: {
 					courseName: '',
 					courseClass: '',
-					cover: ''
-				}
+					cover: '',
+				},
+				file: '',
+				show: true
 			};
 		},
 		methods: {
@@ -37,7 +52,19 @@
 					alert('新增班课成功');
 					_this.$router.push('/');
 				});
+			},
+			//点击图片预览区，即模拟点击文件选择组件
+			handleClick: function() {
+				document.getElementById('coverFile').click();
+			},
+			//图片预览
+			getFile: function() {
+				this.file = event.target.files[0];
+				var windowURL = window.URL || window.webkitURL;
+				this.course.cover = windowURL.createObjectURL(this.file);
+				this.show = false;
 			}
+
 		}
 	};
 </script>
@@ -58,14 +85,72 @@
 		font-size: 14px;
 	}
 
+	.title {
+		font-size: 13px;
+		position: relative;
+		top: -0.625rem;
+	}
+
 	.btn {
+		display: flex;
+	}
+
+	.btn1 {
+		margin-top: 20px;
 		width: 120px;
 		height: 40px;
 		border: 2px solid rgb(0, 187, 221);
 		background-color: #fff;
-		border-radius: 8px;
+		border-radius: 5px;
 		outline: none;
 		color: rgb(0, 187, 221);
 		font-size: 16px;
+		cursor: pointer;
+	}
+
+	.btn2 {
+		margin-left: 200px;
+		width: 120px;
+		height: 40px;
+		margin-top: 20px;
+		cursor: pointer;
+		font-size: 16px;
+		background-color: #fff;
+		border: 2px solid #AAAAAA;
+		outline: none;
+	}
+
+	.btn2:hover {
+		color: #AAAAAA;
+	}
+
+	.preview {
+		width: 150px;
+		height: 150px;
+		border: 2px dashed #aaa;
+		cursor: pointer;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		margin-left: 20px;
+	}
+
+	.icon-plus {
+		width: 60px;
+		height: 60px;
+	}
+
+	.icon-plus img {
+		width: 100%;
+		height: 100%;
+		/* margin-left: auto;
+		margin-right: auto; */
+	}
+
+	.course-cover {
+		width: 100%;
+		height: 100%;
+		margin-left: auto;
+		margin-right: auto;
 	}
 </style>
--- vue-router-demo1/src/components/Task.vue
+++ vue-router-demo1/src/components/Task.vue
@@ -1,21 +1,88 @@
 <template>
-	<!-- 根容器 -->
 	<div class="container">
-		<h2>任务中心</h2>
+		<input type="text" placeholder="请输入班课名称" v-model="courseName" class="input-box" />
+		<input type="text" placeholder="请输入班级" v-model="courseClass" class="input-box" />
+		<p>选择班课封面</p>
+		<div class="preview" @click="handleClick()">
+			<img :src="imgUrl" class="cover" v-if="!show" />
+			<img src="../assets/plus.png" class="icon-plus" v-if="show" />
+			<input type="file" @change="getFile($event)" style="display: none;" id="coverFile" />
+		</div>
+		<button @click="addCourse()" class="btn">确定</button>
 	</div>
 </template>
 
 <script>
-	export default{
-		name: 'Task',
-		data(){
-			return{
-				
-			}
+export default {
+	name: 'NewCourse',
+	data() {
+		return {
+			loginUserId: 1,
+			courseName: '',
+			courseClass: '',
+			file: '',
+			imgUrl: '',
+			show: true
+		};
+	},
+	methods: {
+		//点击图片预览区，即模拟点击文件选择组件
+		handleClick: function() {
+			document.getElementById('coverFile').click();
+		},
+		//图片预览
+		getFile: function() {
+			this.file = event.target.files[0];
+			var windowURL = window.URL || window.webkitURL;
+			this.imgUrl = windowURL.createObjectURL(this.file);
+			this.show = false;
 		}
-	}
-</script>
-
-<style scoped>
 	
+		
+	}
+};
+</script>
+<style scoped>
+.container {
+	display: flex;
+	flex-direction: column;
+	padding-top: 20px;
+	padding-left: 100px;
+	background-color: #fff;
+	margin-top: 20px;
+}
+.input-box {
+	width: 500px;
+	height: 40px;
+	margin-bottom: 40px;
+	font-size: 14px;
+}
+.btn {
+	margin-top: 20px;
+	width: 120px;
+	height: 40px;
+	border: 2px solid rgb(0, 187, 221);
+	background-color: #fff;
+	border-radius: 8px;
+	outline: none;
+	color: rgb(0, 187, 221);
+	font-size: 16px;
+}
+.preview {
+	width: 150px;
+	height: 150px;
+	border: 2px dashed #aaa;
+	cursor: pointer;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+.icon-plus {
+	width: 100px;
+	height: 70px;
+}
+.cover {
+	width: 100%;
+	height: 100%;
+}
 </style>
